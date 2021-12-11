92d98eb6af83f386b79778dff119ed67eda00b48
Uni <1025687605@qq.com>
2021-12-11 16:22:47 +0800

    feat: add rust event

--- src/events.rs
+++ src/events.rs
@@ -1,84 +1,81 @@
-use std::collections::HashMap;
-
 use crate::Listener;
 
-pub struct Events {
+pub struct Events<T> {
   // 监听器
-  listeners: HashMap<String, Vec<Listener>>,
+  pub listeners: Vec<Listener<T>>,
   // 最大的监听器个数
-  max_listeners: i32,
+  pub max_listeners: i32,
 }
 
-impl Events {
+impl<T> Events<T> {
   pub fn new() -> Self {
     Self {
-      listeners: HashMap::new(),
+      listeners: Vec::new(),
       max_listeners: 0,
     }
   }
 
-  pub fn get_event_names(&self) {}
+  pub fn get_event_names(&self) -> Vec<&str> {
+    let mut names = Vec::new();
+    for listener in self.listeners.iter() {
+      names.push(listener.name.as_str());
+    }
 
-  pub fn dispatch_max_listeners() {}
+    names
+  }
 
   // 添加监听器
-  pub fn on(&mut self, event_name: String, listener: Listener) {
-    let listeners = self.listeners.get(&event_name);
-
-    match listeners {
-      Some(v) => {
-        let mut next_listeners = v.clone();
-        next_listeners.push(listener);
-        self.listeners.insert(event_name, next_listeners);
-      }
-      None => {
-        let next_listeners = vec![listener];
-        self.listeners.insert(event_name, next_listeners);
-      }
-    }
+  pub fn on(&mut self, event_name: &str, listener: fn(&T)) {
+    self.listeners.push(Listener {
+      name: String::from(event_name),
+      once: false,
+      listener,
+    })
   }
 
   // 触发监听器, 暂时不支持可变参数
-  pub fn emit(&self, event_name: String) {
-    let listeners = self.listeners.get(&event_name);
+  pub fn emit(&mut self, event_name: &str, value: &T) {
+    let mut remove_listeners_index = Vec::new();
 
-    match listeners {
-      Some(v) => {
-        for val in v.iter() {
-          val()
+    for (index, listener) in self.listeners.iter().enumerate() {
+      if (listener.name.as_str() == event_name) {
+        (listener.listener)(value);
+
+        if listener.once {
+          remove_listeners_index.push(index);
         }
       }
-      None => {}
+    }
+
+    for index in remove_listeners_index.iter() {
+      self.listeners.remove(*index);
     }
   }
 
   // 监听后只触发一次
-  pub fn once() {}
+  pub fn once(&mut self, event_name: &str, listener: fn(&T)) {
+    self.listeners.push(Listener {
+      name: String::from(event_name),
+      once: true,
+      listener,
+    })
+  }
 
   // 移除监听器
-  pub fn off(&mut self, event_name: String, listener: Listener) {
-    let listeners = self.listeners.get(&event_name);
+  pub fn off(&mut self, event_name: &str) {
+    let mut i = 0;
 
-    match listeners {
-      Some(v) => {
-        let mut next_listeners = v.clone();
-        let mut i = 0;
-
-        while i < next_listeners.len() {
-          if listener == v[i] {
-            next_listeners.remove(i);
-          } else {
-            i += 1;
-          }
-        }
-        self.listeners.insert(event_name, next_listeners);
+    while i < self.listeners.len() {
+      if self.listeners[i].name == event_name {
+        self.listeners.remove(i);
+      } else {
+        i += 1;
       }
-      None => {}
     }
   }
 
   // on 的别名
-  pub fn add_listener(&mut self, event_name: String, listener: Listener) {
+  pub fn add_listener(&mut self, event_name: &str, listener: fn(&T)) {
     self.on(event_name, listener);
   }
 
@@ -93,9 +90,11 @@ impl Events {
   }
 
   // off 的别名
-  pub fn remove_listener(&mut self, event_name: String, listener: Listener) {
-    self.off(event_name, listener);
+  pub fn remove_listener(&mut self, event_name: &str) {
+    self.off(event_name);
   }
 
-  pub fn remove_all_listeners() {}
+  pub fn remove_all_listeners(&mut self) {
+    self.listeners.clear();
+  }
 }
--- src/listener.rs
+++ src/listener.rs
@@ -1,1 +1,18 @@
-pub type Listener = fn();
+// 写法和 js 差距很大，所以才定义这样的结构体，比如 js 中的 once 可以这样实现
+/**
+ * function once(eventName, listener) {
+ *  const nextListener = () => {
+ *      listener()
+ *      this.off(eventName, nextListener)
+ *   }
+ *   this.on(eventName, nextListener)
+ * }
+ */
+pub struct Listener<T> {
+  // 事件名称
+  pub name: String,
+  // 是否只监听一次
+  pub once: bool,
+  // 监听器
+  pub listener: fn(&T),
+}
